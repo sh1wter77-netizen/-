abbaf9870fb5f0b59a3b105aa8fb8bd26f109fcb
sh1wter77-netizen <sh1wter.77@gmail.com>
2026-06-17 14:31:21 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+API_TOKEN = '8468368679:AAEuOrhl4QmN3todm8FHS8RzjChz03Ag4FE'
+ADMIN_ID = 6211387728  # Ваш числовой ID из Telegram
+SUPPORT_USERNAME = 'sh1wter'
+PAYMENT_REQUISITES = "Реквизиты: Альфа 2200 1536 9937 5904. Цена: 100 руб/мес."
